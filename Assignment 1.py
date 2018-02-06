bc50151265fb7f89e6c0330b41882220345fe17d
sripadh4 <35394080+sripadh4@users.noreply.github.com>
2018-02-06 16:07:26 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[1]:
+
+PNX = (3/4) ** 5
+PN = (1/4) ** 15
+NFactorial = (20*19*18*17*16)
+XFactorial = (5*4*3*2*1)
+Probability = ((NFactorial/XFactorial)*(PNX)*(PN))
+print (Probability)
+
